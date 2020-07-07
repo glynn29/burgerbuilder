b0b7391c13052a26adebe30c5def2c0140b719a4
glynn29 <glynn_29@hotmail.com>
2020-07-07 18:38:55 -0500

    combined order and BurgerBuilder reducers

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -26,7 +26,7 @@ export const purchaseBurger = (orderData) =>{
         dispatch(purchaseBurgerStart());
         instance.post('/orders.json',orderData)
             .then(res => {
-                 dispatch(purchaseBurgerSuccess(res.data, orderData));
+                 dispatch(purchaseBurgerSuccess(res.data.name, orderData));
                 }
             )
             .catch(error => {
