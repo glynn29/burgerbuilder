ac86bdac36611a36655592e7a2118190df960c21
glynn29 <glynn_29@hotmail.com>
2020-07-14 16:48:01 -0500

    set up actions and reducer for logout

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,6 +5,7 @@ import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
 import classes from './Auth.module.css';
 import * as actions from '../../store/actions/index'
+import Spinner from "../../components/UI/Spinner/Spinner";
 class Auth extends React.Component{
     state = {
         controls: {
@@ -113,7 +114,7 @@ class Auth extends React.Component{
             })
         }
 
-        const form = formElementsArray.map(formElement => (
+        let form = formElementsArray.map(formElement => (
             <Input
                 key={formElement.id}
                 elementType={formElement.config.elementType}
@@ -126,8 +127,18 @@ class Auth extends React.Component{
             />
         ));
 
+        if(this.props.loading){
+            form = <Spinner/>;
+        }
+
+        let errorMessage =null;
+        if (this.props.error){
+            errorMessage = <p>{this.props.error.message}</p>;
+        }
+
         return(
             <div className={classes.Auth}>
+                {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">Submit</Button>
@@ -141,10 +152,17 @@ class Auth extends React.Component{
     }
 }
 
+const mapStateToProps = state => {
+    return{
+        loading: state.auth.loading,
+        error: state.auth.error,
+    };
+};
+
 const mapDispatchToProps = dispatch =>{
     return{
         onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
     };
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
