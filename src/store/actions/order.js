007951c0fd13943e872e8d3aa315d66d5e3f7bef
glynn29 <glynn_29@hotmail.com>
2020-08-17 18:51:49 -0500

    added userid to orders and sorting for orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -62,10 +62,11 @@ export const fetchOrdersStart = () => {
     }
 };
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrdersStart());
-        instance.get('/orders.json?auth=' + token)
+        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+        instance.get('/orders.json' + queryParams)
             .then(res => {
                 const fetchedOrders = [];
                 for(let key in res.data) {
