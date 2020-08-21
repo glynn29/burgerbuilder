66b3c381bc34fbe11cb11f74f18c8bc1dec7a040
glynn29 <glynn_29@hotmail.com>
2020-08-21 18:15:47 -0500

    converted to function components and implemented react hooks

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,48 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import {connect} from "react-redux";
 
 import Aux from "../../hoc/Auxillary";
 import classes from "./Layout.module.css"
 import Toolbar from "../Navigation/ToolBar/Toolbar";
 import SideDrawer from "../Navigation/NavigationItems/SideDrawer/SideDrawer";
-class layout extends React.Component{
-    state={
-        showSideDrawer: false
+const Layout = props => {
+    const [sideDrawerIsVisible, setSideDrawerIsVisible] = useState(false);
+
+    const sideDrawerClosedHandler = () => {
+      setSideDrawerIsVisible(false);
     };
 
-    sideDrawerClosedHandler = () => {
-      this.setState({showSideDrawer: false});
+    const sideDrawerToggleHandler = () => {
+        setSideDrawerIsVisible(!sideDrawerIsVisible);
     };
 
-    sideDrawerToggleHandler = () => {
 
-        this.setState((prevState)=>{
-        return {showSideDrawer: !prevState.showSideDrawer};
-        });
-    };
-
-    render() {
         return(
             <Aux>
                 <div>
                     <Toolbar
-                        isAuth={this.props.isAuthenticated}
-                        drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                        isAuth={props.isAuthenticated}
+                        drawerToggleClicked={sideDrawerToggleHandler}/>
                     <SideDrawer
-                        isAuth={this.props.isAuthenticated}
-                        open={this.state.showSideDrawer}
-                        closed={this.sideDrawerClosedHandler}/>
+                        isAuth={props.isAuthenticated}
+                        open={sideDrawerIsVisible}
+                        closed={sideDrawerClosedHandler}/>
 
                 </div>
                 <main className={classes.Content}>
-                    {this.props.children}
+                    {props.children}
                 </main>
             </Aux>
         );
-    }
-
-
-}
+};
 
 const mapPropsToState = state => {
     return{
@@ -50,4 +42,4 @@ const mapPropsToState = state => {
     }
 };
 
-export default connect(mapPropsToState)(layout);
+export default connect(mapPropsToState)(Layout);
